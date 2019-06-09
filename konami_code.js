99f215fe751b9baa21e75ccfe587f568f78ef3f6
Rafael Batista <RafaelB24@yahoo.com>
2019-06-08 17:55:33 -0700

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let input = [];
+
+function checkCode() {
+  for (let i = 0; i < code.length; i++) {
+    if (code[i] !== input[i]) {
+      return false;
+    }
+  }
+}
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
 
+    if (input.length >= 10) {
+       input.shift();
+       input.push(key);
+    }
+    else {
+      input.push(key);
+    }
+
+    if ( checkCode() !== false) {
+      alert('99 Lives!');
+    }
+
+  });
 }
